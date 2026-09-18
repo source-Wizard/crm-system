c6ed48b59d98ab6328ed006af67382275c8652e8
userName <userEmail>
2026-09-18 15:11:25 +0530

    Optimize for Vercel: bundle staticfiles with WhiteNoise and streamline vercel.json

--- build_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Installing project dependencies..."
-python3 -m pip install -r requirements.txt
-
-echo "Collecting static files..."
-python3 manage.py collectstatic --noinput --clear
